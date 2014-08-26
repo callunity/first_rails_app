b4894fa1756942f449b08b7bd8022f90be7f3f0f
callunity <heather.e.armstrong@gmail.com>
2014-08-26 15:51:29 -0400

    Refactor routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :pictures
+
   root :to => 'pictures#index'
-  get 'pictures' => 'pictures#index'
-
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id' => 'pictures#show', as: "picture"
-  get 'pictures/:id/edit' => 'pictures#edit', as: "edit_picture"
 
   patch 'pictures/:id' => 'pictures#update'
 
